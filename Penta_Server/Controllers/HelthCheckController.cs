6a85a4a1d701d089bc73f9d335bc2a72965ab756
ivan.suvorin <ivan.suvorin@indusoft.ru>
2026-05-11 16:04:52 +0700

    добавил контрол для чека

---
+++ Penta_Server/Controllers/HelthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Penta_Server.Controllers
+{
+    [Route("WorkStatus")]
+    [ApiController]
+    public class HelthCheckController : ControllerBase
+    {
+        [HttpGet("Check")]
+        public IActionResult AmAlive()
+        {
+            return Ok();
+        }
+    }
+}
